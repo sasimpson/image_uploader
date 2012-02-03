8331936b1300df0d49fc2ec235ec91f490751139
Scott Simpson <sasimpson@gmail.com>
2012-02-03 16:51:22 -0600

    adding bootstrap 2.0, adding delete method, adding tests

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-app.py
-
-Created by Scott Simpson on 2012-01-13.
-"""
 import json
 import cloudfiles
 
@@ -15,9 +10,13 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('IMGR_SETTINGS')
 
-def cf_get_dirs():
+def cf_connect():
     cf = cloudfiles.Connection(app.config['CF_USER'], app.config['CF_KEY'])
     container = cf.get_container(app.config['CONTAINER'])
+    return container
+
+def cf_get_dirs():
+    container = cf_connect()
     dirs = container.list_objects(delimiter='/', prefix=app.config['PREFIX'])
     return dirs
 
@@ -33,8 +32,7 @@ def get_new():
 @app.route('/new', methods=["POST"])
 def post_new():
     """create/upload new image"""
-    cf = cloudfiles.Connection(app.config['CF_USER'], app.config['CF_KEY'])
-    container = cf.get_container(app.config['CONTAINER'])
+    container = cf_connect()
     image = request.files['image']
     if image:
         cf_image = container.list_objects(prefix="%s/" % image.filename, delimiter="/")
@@ -48,28 +46,43 @@ def post_new():
     else:
         return "need image."
     
-@app.route('/dir/new', methods=["POST"])
-def dir_new():
-    """docstring for dir_new"""
-    return "dir_new"
-
 @app.route('/list')
 def list():
     """docstring for list"""
     marker = request.args.get('marker', '')
-    cf = cloudfiles.Connection(app.config['CF_USER'], app.config['CF_KEY'])
-    container = cf.get_container(app.config['CONTAINER'])
+    container = cf_connect()
     images = container.get_objects(prefix=app.config['PREFIX'], limit=100, marker=marker)
-    print container.list_objects()
     if len(images) >= 100:
         return render_template('list.html', images=images, prev=images[0].name, next=images[-1].name, cname=app.config['CNAME'])
     else:
         return render_template('list.html', images=images, cname=app.config['CNAME'])
 
-@app.route('/i/<path:image>', methods=['GET'])
-def image():
-    """docstring for image"""
-    pass
-    
+@app.route('/delete/<path:image_name>', methods=['GET'])
+@app.route('/<path:image_name>', methods=['DELETE'])
+def delete(image_name):
+    if image_name:
+        container = cf_connect()
+        try:
+            image = container.get_object(image_name)
+            container.delete_object(image.name)
+            return redirect(url_for('list'))
+        except cloudfiles.errors.NoSuchObject:
+            abort(404)
+    else:
+        abort(404)
+
+@app.route('/help', methods=['GET'])
+def help():
+    abort(404)    
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('4xx.html', code='404', message="Sorry, we couldn't find what you were looking for."), 404
+
+@app.errorhandler(405)
+def page_not_found(error):
+    return render_template('4xx.html', code=405, message="That method is not allowed here."), 405
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- image_uploader_tests.py
+++ image_uploader_tests.py
@@ -3,10 +3,42 @@ import app
 import unittest
 import tempfile
 
+class Object(object):
+    """mock for Object"""
+    def __init__(self, **kw):
+        for k in kw.keys():
+            setattr(self, k, kw[k])
+    def public_uri(self):
+        return self.pub_uri
+        
+
+class Container(object):
+    """mock for Container"""
+    def __init__(self, **kw):
+        for k in kw.keys():
+            setattr(self, k, kw[k])
+        
+    def get_objects(self, **kw):
+        if self.empty:
+            return []
+        else:
+            return [
+                Object(name="foo.jpg", size=65432, content_type="image/jpeg", pub_uri="http://foo.bar.com/foo.jpg"),
+                Object(name="bar.jpg", size=12345, content_type="image/jpeg", pub_uri="http://foo.bar.com/bar.jpg")
+            ]
+    
+
+def cf_connect_stub():
+    return Container(name="foo", empty=False)
+
+def cf_connect_empty_stub():
+    return Container(name="foo", empty=True)
+
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
         app.app.config['TESTING'] = True
+        app.app.config['CNAME'] = 'http://foo.com'
         self.app = app.app.test_client()
 
     def tearDown(self):
@@ -16,8 +48,29 @@ class AppTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert "Image Uploader" in rv.data
     
-    def test_new_renders_template(self):
-        pass
-        
+    def test_list_no_content(self):
+        old_cf_connect = app.cf_connect
+        app.cf_connect = cf_connect_empty_stub
+        rv = self.app.get('/list')
+        assert "No images found." in rv.data
+        app.cf_connect = old_cf_connect
+
+    def test_list_with_content(self):
+        old_cf_connect = app.cf_connect
+        app.cf_connect = cf_connect_stub
+        rv = self.app.get('/list')
+        assert "foo.jpg" in rv.data
+        assert "bar.jpg" in rv.data
+        app.cf_connect = old_cf_connect
+    
+    def test_new_get_form(self):
+        rv = self.app.get('/new')
+        assert "submit" in rv.data
+        assert "Image File" in rv.data
+    
+    def test_new_post_form(self):
+        rv = self.app.post()
+    
+
 if __name__ == '__main__':
     unittest.main()
